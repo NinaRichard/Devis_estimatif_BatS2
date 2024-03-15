ea4617a674fbe3925a0db95d89e4966a0083b2d3
NinaRichard <163274414+NinaRichard@users.noreply.github.com>
2024-03-15 10:12:39 +0100

    Add files via upload

---
+++ projet info-20240313T084751Z-001/projet info/B3-projet/src/main/java/insa/wencker/b3/projet/B3Projet.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package insa.wencker.b3.projet;
+
+/**
+ *
+ * @author emeli
+ */
+public class B3Projet {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
